175fe6a11a5db39378b11d2868334f6c421a1499
Jackson Zaloumis <>
2019-01-10 10:55:20 +1100

    Project init

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Platform, Text, View } from "react-native";
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+interface Props {}
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+        <Text>To get started, edit App.tsx</Text>
+        <Text>{instructions}</Text>
+      </View>
+    );
+  }
+}
