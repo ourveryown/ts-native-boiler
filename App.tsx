3e8a13fb83e955640bbcc3d31d690857113f2521
Jackson Zaloumis <>
2019-01-10 15:41:57 +1100

    Added Apollo Client for API

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
 
-// APP
-import { createAppContainer } from "react-navigation";
-import Main from "./app/domains/app/navigation";
+// API
+import { ApolloProvider } from "react-apollo";
+import Client from "./app/config/api";
 
 // STYLED COMPONENTS
 import { ThemeProvider } from "styled-components";
 import theme from "./app/theme";
+
+// APP
+import { createAppContainer } from "react-navigation";
+import Main from "./app/domains/app/navigation";
 
 const AppContainer = createAppContainer(Main);
 
 export default class App extends Component {
   public render() {
     return (
-      <ThemeProvider theme={theme}>
-        <AppContainer />
-      </ThemeProvider>
+      <ApolloProvider client={Client}>
+        <ThemeProvider theme={theme}>
+          <AppContainer />
+        </ThemeProvider>
+      </ApolloProvider>
     );
   }
 }
---
+++ app/config/api.ts
@@ -0,0 +1,19 @@
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { createHttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+
+const uri = "http://localhost:4000/";
+
+const httpLink = createHttpLink({
+  uri
+});
+
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([httpLink])
+});
+
+export default client;
